16f2e50bdf79ffd88a7df19a15d5addf2501e7e3
Juhana Tamminen <juhanatamminen@81-197-60-199.elisa-laajakaista.fi>
2024-10-18 17:15:07 +0300

    Edited app and ran npm install

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Dogwatch heart Vite + React</h1>
+      <h1>Our dog loving community starts from here!</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
