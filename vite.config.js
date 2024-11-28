2ea017f8992eaa02b3333e5d48b69cecf1d83958
Juhana Tamminen <juhanatamminen@81-197-60-190.elisa-laajakaista.fi>
2024-11-28 23:35:13 +0200

    vite base change

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/watchdog/",
+  base: "/dogwatch/",
   build: {
     sourcemap: true, // Enable source maps for the build
   },
